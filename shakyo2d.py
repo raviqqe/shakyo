1ec3df2e894249be4aa135733f0422e1fe54bf3b
Raven Iqqe <raviqqe@gmail.com>
2015-11-02 00:31:10 +0900

    Make the screen simpler

--- shakyo2d.py
+++ shakyo2d.py
@@ -89,16 +89,14 @@ class Console:
         if not self.__console._window.getyx()[1] < self.screen_width: return
         self.__console._window.addch(ord(char), attr)
 
-      def delete_char(self):
-        y, x = self.__console._window.getyx()
-        if x == 0: return
-        self.__console._window.delch(y, x - 1)
-
       def print_line(self, line_pos, text):
         assert len(text) <= self.screen_width
         self.__console._window.move(line_pos, 0)
         self.__console._window.clrtoeol()
         self.__console._window.addstr(line_pos, 0, text)
+
+      def move(self, y, x):
+        self.__console._window.move(y, x)
 
       def clear(self):
         self.__console._window.clear()
@@ -163,8 +161,11 @@ class TypingGame:
 
   def __delete_char(self):
     if len(self.__input_text) == 0: return
+    current_cursor_position = len(self.__input_text)
+    self.__api.move(self.__geometry.current_line, current_cursor_position - 1)
+    self.__api.add_char(self.__example_text[0][len(self.__input_text) - 1])
+    self.__api.move(self.__geometry.current_line, current_cursor_position - 1)
     self.__input_text = self.__input_text[:-1]
-    self.__api.delete_char()
 
   def __cheat(self):
     if self.__example_text[1] == None:
@@ -174,53 +175,35 @@ class TypingGame:
 
   def __initialize_screen(self):
     self.__print_all_example_text()
-    self.__draw_partitions()
     self.__clear_input_text()
 
   def __scroll(self):
     self.__api.scroll()
-    self.__api.print_line(self.__geometry.current_example_line - 2,
-                          self.__example_text[0])
     del self.__example_text[0]
-    self.__print_current_example_text()
     self.__print_bottom_example_text()
-    self.__draw_partitions()
     self.__clear_input_text()
 
   def __clear_input_text(self):
     self.__input_text = ""
-    self.__api.print_line(self.__geometry.input_line, self.__input_text)
-
-  def __print_current_example_text(self):
-    self.__api.print_line(self.__geometry.current_example_line,
-                          self.__example_text[0])
+    self.__api.move(self.__geometry.current_line, 0)
 
   def __print_bottom_example_text(self):
-    index = self.__geometry.bottom_line - self.__geometry.next_example_line + 1
+    index = self.__geometry.bottom_line - self.__geometry.current_line
     if self.__example_text[index] != None:
       self.__api.print_line(self.__geometry.bottom_line,
                             self.__example_text[index])
 
   def __print_all_example_text(self):
-    self.__print_current_example_text()
-    for index in range(1, 1 + (self.__geometry.bottom_line
-                               - self.__geometry.next_example_line + 1)):
+    for index in range(self.__geometry.bottom_line
+                       - self.__geometry.current_line):
       if self.__example_text[index] == None: break
-      self.__api.print_line(self.__geometry.next_example_line + (index - 1),
+      self.__api.print_line(self.__geometry.current_line + index,
                             self.__example_text[index])
-
-  def __draw_partitions(self):
-    PARTITION_CHAR = '-'
-    partition = PARTITION_CHAR * self.__api.screen_width
-    self.__api.print_line(self.__geometry.current_example_line - 1, partition)
-    self.__api.print_line(self.__geometry.input_line + 1, partition)
 
 
 class Geometry:
   def __init__(self, screen_height):
-    self.input_line = screen_height // 2
-    self.current_example_line = self.input_line - 1
-    self.next_example_line = self.input_line + 2
+    self.current_line = (screen_height - 1) // 2
     self.bottom_line = screen_height - 1
 
 
