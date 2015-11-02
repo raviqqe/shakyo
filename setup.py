65185cc76fff57bb09f4590ba8e61cfc432a4067
raviqqe <raviqqe@gmail.com>
2015-11-02 11:03:58 +0000

    Rename script

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setuptools.setup(
     author="raviqqe",
     author_email="raviqqe@gmail.com",
     url="http://github.com/raviqqe/shakyo/",
-    scripts=[shakyo.__name__],
+    py_modules=[shakyo.__name__],
+    entry_points={"console_scripts" : ["shakyo=shakyo:main"]},
     install_requires=["text_unidecode", "validators"],
     classifiers=[
         "Development Status :: 3 - Alpha",
