25fbe5416ed6965557ec5d7238eb73ecfdb89f99
Raven Iqqe <raviqqe@gmail.com>
2015-11-07 22:19:53 +0900

    Fix the way to specify the package's name and version in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import setuptools
 import sys
 
@@ -7,18 +8,23 @@ if not ((sys.version_info.major >= 3 and sys.version_info.minor >= 5)
     or sys.version_info.major > 3):
   exit("Sorry, Python's version must be later than 3.5.")
 
-import shakyo
 
+PACKAGE_NAME = "shakyo"
+
+with open("shakyo.py") as f:
+  lines = f.readlines()
+version = next(re.match(r"__version__\s*=\s*\"((\d|\.)*)\"", line)
+               for line in lines if line.startswith("__version__")).group(1)
 
 setuptools.setup(
-    name=shakyo.__name__,
-    version=shakyo.__version__,
+    name=PACKAGE_NAME,
+    version=version,
     description="a tool to learn about something just by copying it by hand",
     license="Public Domain",
     author="raviqqe",
     author_email="raviqqe@gmail.com",
     url="http://github.com/raviqqe/shakyo/",
-    py_modules=[shakyo.__name__],
+    py_modules=[PACKAGE_NAME],
     entry_points={"console_scripts" : ["shakyo=shakyo:main"]},
     install_requires=["text_unidecode", "validators"],
     classifiers=[
