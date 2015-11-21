b7ca891c32cccee453cba666ec0d7a37be17b34b
Raven Iqqe <raviqqe@gmail.com>
2015-11-21 21:58:59 +0900

    Fix randomness of text colors
    If the nearest color for a rgb tuple is stored in the rgb2attr dictionary,
    it is also used as a pre-existent color next time.
    Then, the determined colors depends on the order of rgb tuples used as arguments of
    get_best_match().

--- xorcise/attribute.py
+++ xorcise/attribute.py
@@ -16,23 +16,25 @@ class RenditionAttribute(enum.IntEnum):
 
 
 class ColorAttribute:
+  __colors = []
+  __rgb2attr = {}
+
   @classmethod
   def initialize(cls):
     if not curses.has_colors(): return
 
-    cls.__rgb2attr = {}
     for color_index in range(curses.COLORS):
       pair_index = color_index + 1
       if pair_index >= curses.COLOR_PAIRS: break
       curses.init_pair(pair_index, color_index, -1)
       rgb = tuple(color * 255 // 1000
                   for color in curses.color_content(color_index))
-      cls.__rgb2attr[rgb] = curses.color_pair(pair_index)
+      cls.__colors.append((rgb, curses.color_pair(pair_index)))
+    cls.__colors.sort()
 
   @classmethod
   def get_best_match(cls, rgb):
-    assert len(rgb) == 3
-    assert all(0 <= color < 256 for color in rgb)
+    assert cls.__is_valid_rgb(rgb)
 
     if not curses.has_colors():
       return curses.A_NORMAL
@@ -46,15 +48,22 @@ class ColorAttribute:
 
   @classmethod
   def __find_best_match(cls, new_rgb):
+    assert cls.__is_valid_rgb(new_rgb)
     matched_attr = curses.color_pair(0) # white on black
     min_distance = cls.__color_distance((0, 0, 0), (255, 255, 255))
-    for rgb, attr in cls.__rgb2attr.items():
+    for rgb, attr in cls.__colors:
       distance = cls.__color_distance(new_rgb, rgb)
       if distance < min_distance:
         min_distance = distance
         matched_attr = attr
     return matched_attr
 
-  @staticmethod
-  def __color_distance(rgb1, rgb2):
+  @classmethod
+  def __color_distance(cls, rgb1, rgb2):
+    assert cls.__is_valid_rgb(rgb1) and cls.__is_valid_rgb(rgb2)
     return sum((color1 - color2) ** 2 for color1, color2 in zip(rgb1, rgb2))
+
+  @staticmethod
+  def __is_valid_rgb(rgb):
+    return len(rgb) == 3 and all(isinstance(color, int) and 0 <= color < 256
+                                 for color in rgb)
