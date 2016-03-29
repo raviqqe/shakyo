1857f657e041a350411800fde877b8ca79d128ed
Raven Iqqe <raviqqe@gmail.com>
2016-03-29 13:34:07 +0900

    Simplify the judgement conditions of if the input character is correct or not

--- shakyo/shakyo.py
+++ shakyo/shakyo.py
@@ -47,7 +47,8 @@ class Shakyo:
       elif ck.is_printable_char(char) \
            and (self._input_line + ck.Character(char)).width \
                + self.CURSOR_WIDTH <= self._console.screen_width:
-        self._input_line += ck.Character(char, self._next_char_attr(char))
+        self._input_line += ck.Character(char,
+                                         self._next_input_char_attr(char))
 
   def _update_input_line(self):
     self._console.print_line(self._geometry.y_input, self._example_lines[0])
@@ -88,27 +89,31 @@ class Shakyo:
       self._console.print_line(self._geometry.y_input + index,
                                self._example_lines[index])
 
-  def _next_char_attr(self, char):
+  def _next_input_char_attr(self, char):
     attr_correct = self._console.decoration_attrs.normal
     attr_wrong = self._console.decoration_attrs.reverse
-
-    normalized_input_line = self._input_line.normalized
-    normalized_example_line = self._example_lines[0].normalized
-    if len(normalized_input_line) >= len(normalized_example_line):
-      return attr_wrong
     return (attr_correct if self._is_correct_char(char) else attr_wrong) \
-           | normalized_example_line[min(len(normalized_input_line),
-                                         len(normalized_example_line) - 1)] \
-                                         .attr
+           | self._next_example_char_attr
+
+  @property
+  def _next_example_char_attr(self):
+    normalized_example_line = self._example_lines[0].normalized
+    return normalized_example_line[min(len(self._input_line.normalized),
+                                       len(normalized_example_line) - 1)].attr
 
   def _is_correct_char(self, char):
-    new_input_line = self._input_line + ck.Character(char)
-    for index in range(len(self._input_line.normalized),
-                       len(new_input_line.normalized)):
-      if index >= len(self._example_lines[0].normalized) \
-         or new_input_line.normalized[index] \
-            != self._example_lines[0].normalized[index]:
-        return False
+    normalized_input_line = self._input_line.normalized
+    normalized_example_line = self._example_lines[0].normalized
+    normalized_new_input_line = (self._input_line + ck.Character(char)) \
+                                .normalized
+
+    if len(normalized_new_input_line) > len(normalized_example_line):
+      return False
+    new_char_range = slice(len(normalized_input_line),
+                           len(normalized_new_input_line))
+    if normalized_new_input_line[new_char_range] \
+       != normalized_example_line[new_char_range]:
+      return False
     return True
 
 
