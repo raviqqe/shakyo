846749838c2b0b79ce99ff1234116a20eedf2d53
Yota Toyama <raviqqe@gmail.com>
2016-06-06 10:21:45 +0900

    Create util.sequence_to_string()

--- shakyo/util.py
+++ shakyo/util.py
@@ -2,3 +2,7 @@ def interpret_string_rgb(string_rgb):
   assert len(string_rgb) == 6
   int_rgb = int(string_rgb, 16)
   return (int_rgb >> 16 & 0xff, int_rgb >> 8 & 0xff, int_rgb & 0xff)
+
+
+def sequence_to_string(sequence):
+  return ", ".join(sorted(str(elem) for elem in sequence))
