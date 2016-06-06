f49757518833785c15e5c08b91035d67ff4a91dd
Yota Toyama <raviqqe@gmail.com>
2016-06-06 15:44:26 +0900

    Refactor shakyo.__main__

--- shakyo/__main__.py
+++ shakyo/__main__.py
@@ -12,30 +12,42 @@ from . import log
 
 
 
-# main routine
+def get_example_lines(example_path,
+                      console,
+                      *,
+                      lexer_name,
+                      style_name,
+                      colorize,
+                      decorate):
+  example_text = path_to_x.path_to_text(example_path)
+
+  return text_to_lines.text_to_lines(
+      example_text,
+      console,
+      lexer=pygments_util.guess_lexer(
+          lexer_name=lexer_name,
+          filename=path_to_x.path_to_filename(example_path),
+          text=example_text),
+      style_name=style_name,
+      colorize=colorize,
+      decorate=decorate)
+
 
 def main():
   args = arggetter.get_args()
 
   if not sys.stdout.isatty(): log.error("stdout is not a tty.")
 
-  filename = path_to_x.path_to_filename(args.example_path)
-  example_text = path_to_x.path_to_text(args.example_path)
-
   with ck.Console(asciize=args.asciize,
                   spaces_per_tab=args.spaces_per_tab,
                   background_rgb=args.background_rgb) as console:
-    example_lines = text_to_lines.text_to_lines(
-        example_text,
-        console,
-        lexer=pygments_util.guess_lexer(lexer_name=args.lexer_name,
-                                        filename=filename,
-                                        text=example_text),
-        style_name=args.style_name,
-        colorize=args.colorize,
-        decorate=args.decorate)
-
-    shakyo.Shakyo(console, example_lines).do()
+    shakyo.Shakyo(console,
+                  get_example_lines(args.example_path,
+                                    console,
+                                    lexer_name=args.lexer_name,
+                                    style_name=args.style_name,
+                                    colorize=args.colorize,
+                                    decorate=args.decorate)).do()
 
 
 if __name__ == "__main__":
